d36bec7ca58b4df0bb9d45a1a0556e7f68d9daf8
Matheus Lima <matheusml90@gmail.com>
2015-10-09 14:15:05 -0300

    Adding gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify');
 
 gulp.task('default', function() {
     return gulp.src('app.js')
         .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename('ng-valid.min.js'))
         .pipe(gulp.dest('dist/'))
         .pipe(notify({ message: 'Task is completed' }));
 });
